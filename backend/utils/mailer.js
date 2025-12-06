30f0f9e3f7623da2b9517fc9989b6333300b127b
kushagra2207 <fantastick22072006@gmail.com>
2025-12-06 14:38:13 +0530

    add logs mailer.js for debugging

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,12 +1,21 @@
 const nodemailer = require('nodemailer')
 const { google } = require('googleapis')
 
+const SENDER_EMAIL = process.env.EMAIL
 const CLIENT_ID = process.env.GMAIL_CLIENT_ID
 const CLIENT_SECRET = process.env.GMAIL_CLIENT_SECRET
 const REFRESH_TOKEN = process.env.GMAIL_REFRESH_TOKEN
-const SENDER_EMAIL = process.env.EMAIL
 
-const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, 'urn:ietf:wg:oauth:2.0:oob')
+console.log("MAILER CONFIG:");
+console.log("  SENDER_EMAIL:", SENDER_EMAIL);
+console.log("  CLIENT_ID set:", !!CLIENT_ID);
+console.log("  CLIENT_SECRET set:", !!CLIENT_SECRET);
+console.log("  REFRESH_TOKEN set:", !!REFRESH_TOKEN);
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID, 
+  CLIENT_SECRET
+)
 
 oAuth2Client.setCredentials({
   refresh_token: REFRESH_TOKEN
@@ -14,8 +23,15 @@ oAuth2Client.setCredentials({
 
 async function sendEmail(to, subject, htmlOrText) {
   try {
-    const accessTokenObj = await oAuth2Client.getAccessToken();
-    const accessToken = accessTokenObj.token || accessTokenObj;
+    const accessTokenResponse = await oAuth2Client.getAccessToken();
+    const accessToken = accessTokenResponse?.token || accessTokenResponse;
+
+    if (!accessToken) {
+      console.error("Mailer: no access token received");
+      throw new Error("Failed to obtain access token from Google");
+    }
+
+    console.log("Mailer: got access token, creating transporter...")
 
     const transporter = nodemailer.createTransport({
       service: 'gmail',
@@ -26,7 +42,9 @@ async function sendEmail(to, subject, htmlOrText) {
         clientSecret: CLIENT_SECRET,
         refreshToken: REFRESH_TOKEN,
         accessToken: accessToken
-      }
+      },
+      connectionTimeout: 15000,
+      socketTimeout: 15000
     });
 
     const mailOptions = {
@@ -36,9 +54,12 @@ async function sendEmail(to, subject, htmlOrText) {
       html: htmlOrText
     };
 
+    console.log("Mailer: sending email to", to);
     const result = await transporter.sendMail(mailOptions);
+    console.log("Mailer: email sent, messageId:", result.messageId);
     return result;
   } catch (err) {
+    console.error("Mailer: error while sending email:", err);
     throw err;
   }
 }
